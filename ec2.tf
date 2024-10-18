cbc2a00a3ae3129c0e0580b47f1582709de2d451
SaiLakshmi-github <142158089+SaiLakshmi-github@users.noreply.github.com>
2024-10-18 22:53:03 +0530

    added ec2

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "db" {
+  ami = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "DB"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh"
+  
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    tags = {
+    Name = "allow_ssh"
+    CreatedBy = "Sai Lakshmi"
+  }
+}
