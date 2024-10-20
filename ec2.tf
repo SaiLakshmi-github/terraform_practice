7529619fbb429efb3daee37114dd8002fd994d96
SaiLakshmi-github <142158089+SaiLakshmi-github@users.noreply.github.com>
2024-10-20 22:59:00 +0530

    variables

--- ec2.tf
+++ ec2.tf
@@ -1,33 +1,34 @@
+# resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
-  ami = "ami-09c813fb71547fc4f"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  instance_type = "t3.micro"
 
-  tags = {
-    Name = "DB"
-  }
+    ami = var.image_id
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = var.instance_type
+    # left side things are called as arguements, right side are values.
+    tags = var.tags
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow ssh"
-  
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+    name = var.sg_name
+    description = var.sg_description
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+    # this is block
+    ingress {
+        from_port        = var.ssh_port
+        to_port          = var.ssh_port
+        protocol         = var.protocol
+        cidr_blocks      = var.allowed_cidr
+    }
+
+    egress {
+        from_port        = 0 # from 0 to 0 means, opening all protocols
+        to_port          = 0
+        protocol         = "-1" # -1 all protocols
+        cidr_blocks      = var.allowed_cidr
+    }
 
     tags = {
-    Name = "allow_ssh"
-    CreatedBy = "Sai Lakshmi"
-  }
+        Name = "allow_ssh"
+        CreatedBy = "Sailakshmi"
+    }
 }
